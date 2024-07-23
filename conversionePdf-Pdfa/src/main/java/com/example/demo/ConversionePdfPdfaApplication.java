5db3723244237571451220d6e1bf99efe5f7a162
Romolo1964 <fiorenzaromolo4@gmail.com>
2024-07-23 08:15:37 +0200

    conversionePdf-Pdfa

---
+++ conversionePdf-Pdfa/src/main/java/com/example/demo/ConversionePdfPdfaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionePdfPdfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversionePdfPdfaApplication.class, args);
+	}
+
+}
