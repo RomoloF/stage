e386e6f956342ed3876168aedfbdb79819731ef3
Romolo1964 <fiorenzaromolo4@gmail.com>
2024-07-23 17:22:24 +0200

    Aggiornato e funzionante progetto per convertire i file pdf a pdf/a.

--- pdf/src/main/java/com/example/demo/model/SpiretoPDFA.java
+++ pdf/src/main/java/com/example/demo/model/SpiretoPDFA.java
@@ -4,12 +4,14 @@ import com.spire.pdf.conversion.PdfStandardsConverter;
 
 public class SpiretoPDFA {
     public static void main(String[] args) {
-        String inputFile = "/home/romolofiorenza/stage/spire/src/main/java/pdf/Mod Ries-ac-contr-20191024.pdf";
+        String inputFile = "/home/romolofiorenza/git/pdf/pdf/src/main/java/com/example/demo/model/L7_SpringBoot.pdf";
         String outputFile = inputFile+"ConvertitoPDFA"+".pdf";
         PdfStandardsConverter ps = new PdfStandardsConverter(inputFile);
         ps.toPdfA3B(outputFile);
+
+        System.out.println("Conversione completata!");
+        System.out.println("Il file è >>>>  "+outputFile);
     }
 }
-    
 
-// Usage: java SpiretoPDFA <input-pdf-file> <output-pdf-file>
+
